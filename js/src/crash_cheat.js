d555705a1b366bcb10af9914a4523c06a722e29d
Cenergy <673598118@qq.com>
2019-10-04 21:39:46 +0800

    Site updated: 2019-10-04 21:39:45

--- js/src/crash_cheat.js
+++
@@ -1,3 +0,0 @@
-// build time:Thu Sep 26 2019 23:40:44 GMT+0800 (GMT+08:00)
-const OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
-//rebuild by neat 
