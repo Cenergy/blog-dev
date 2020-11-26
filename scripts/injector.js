e3c3b6e678053fe2100f5cb6dada734835697eb8
gonghaineng <gonghaineng@genew.com>
2020-11-26 09:55:21 +0800

    glsl使用注入器,避免全局引入相关js和css

---
+++ scripts/injector.js
@@ -0,0 +1,11 @@
+const { root: siteRoot = "/" } = hexo.config;
+
+// layout为glsl的时候导入这些js与css
+hexo.extend.injector.register(
+  "body_end",
+  `<link defer rel="stylesheet" href="${siteRoot}styles/glslEditor.css" />
+  <script defer src="${siteRoot}js/glslCanvas.js"></script>
+  <script defer src="${siteRoot}js/glslEditor.js"></script>
+  <script defer src="${siteRoot}js/glslPost.js"></script>`,
+  "glsl"
+);
