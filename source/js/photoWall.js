ae1cf9fc780baaff6923e327a303fd02aeaa9285
cenergy <673598118@qq.com>
2021-10-30 22:34:33 +0800

    使用图片管理

--- source/js/photoWall.js
+++ source/js/photoWall.js
@@ -1,5 +1,5 @@
-var imgDataPath = "/blog/photos/photos.json"; //图片名称高宽信息json文件路径
-var imgPath = "https://cdn.jsdelivr.net/gh/Cenergy/images/gallery/"; //图片访问路径
+var imgDataPath = "https://api.aigisss.com/resources/galleries"; //图片名称高宽信息json文件路径
+var imgPath = "https://api.aigisss.com/media"; //图片访问路径
 var imgMaxNum = 50; //图片显示数量
 
 var windowWidth =
@@ -12,15 +12,36 @@ if (windowWidth < 768) {
   var imageWidth = 250; //图片显示宽度
 }
 
+function getFileName(path) {
+  var pos1 = path.lastIndexOf("/");
+  var pos2 = path.lastIndexOf("\\");
+  var pos = Math.max(pos1, pos2);
+  if (pos < 0) {
+    return path;
+  } else {
+    let tempPath = path.substring(pos + 1);
+    return tempPath.substring(0, tempPath.lastIndexOf("."));
+  }
+}
+
 const photo = {
   page: 1,
   offset: imgMaxNum,
   init: function () {
     var that = this;
-    $.getJSON(imgDataPath, function (data) {
-      that.render(that.page, data);
+    $.getJSON(imgDataPath, function (res) {
+      const { data } = res;
+      const { galleries, photos } = data;
+      const realGalleries = galleries
+        .map((gallery) => {
+          const { id } = gallery;
+          gallery.children = photos[id] || [];
+          return gallery;
+        })
+        .filter((g) => g.children.length);
+      that.render(that.page, realGalleries);
       //that.scroll(data);
-      that.eventListen(data);
+      that.eventListen(realGalleries);
     });
   },
   constructHtml(options) {
@@ -32,14 +53,15 @@ const photo = {
       imgPath,
       imgName,
       imgNameWithPattern,
+      thumbImage,
     } = options;
     const htmlEle = `<div class="card lozad" style="width:${imageWidth}px">
                   <div class="ImageInCard" style="height:${
                     (imageWidth * imageY) / imageX
                   }px">
-                    <a data-fancybox="gallery" href="${imgPath}${name}/${imgNameWithPattern}"
-                          data-caption="${imgName}" title="${imgName}">
-                            <img  class="lazyload" data-src="${imgPath}${name}/${imgNameWithPattern}"
+                    <a data-fancybox="gallery" href="${imgPath}/${thumbImage}"
+                          data-caption="${imgName}" title="${name}">
+                            <img  class="lazyload" data-src="${imgPath}/${imgNameWithPattern}"
                             src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
                             onload="lzld(this)"
                             lazyload="auto">
@@ -69,11 +91,14 @@ const photo = {
         </li>`;
     });
     const [initData = {}] = data;
-    const { children = [],name } = initData;
+    const { children = [], name } = initData;
     children.forEach((item, index) => {
-      imgNameWithPattern = item.split(" ")[1];
-      imgName = imgNameWithPattern.split(".")[0];
-      imageSize = item.split(" ")[0];
+      const { size, image_thumb, image_webp, description } = item;
+
+      imgNameWithPattern = image_thumb;
+      thumbImage = image_webp;
+      imgName = description || getFileName(image_thumb);
+      imageSize = size.split(" ")[0];
       imageX = imageSize.split(".")[0];
       imageY = imageSize.split(".")[1];
       let imgOptions = {
@@ -84,6 +109,7 @@ const photo = {
         imgName,
         imgPath,
         imgNameWithPattern,
+        thumbImage,
       };
       li += this.constructHtml(imgOptions);
     });
@@ -109,12 +135,14 @@ const photo = {
       $(".ImageGrid").empty();
       const selectId = $(e.target).attr("photo-uuid");
       const selectedData = data.find((data) => data.name === selectId) || {};
-      const { children,name } = selectedData;
+      const { children, name } = selectedData;
       let li = "";
       children.forEach((item, index) => {
-        imgNameWithPattern = item.split(" ")[1];
-        imgName = imgNameWithPattern.split(".")[0];
-        imageSize = item.split(" ")[0];
+        const { size, image_thumb, image_webp, description } = item;
+        imgNameWithPattern = image_thumb;
+        thumbImage = image_webp;
+        imgName = description || getFileName(image_thumb);
+        imageSize = size.split(" ")[0];
         imageX = imageSize.split(".")[0];
         imageY = imageSize.split(".")[1];
         let imgOptions = {
@@ -125,6 +153,7 @@ const photo = {
           imgName,
           imgPath,
           imgNameWithPattern,
+          thumbImage,
         };
         li += self.constructHtml(imgOptions);
       });
